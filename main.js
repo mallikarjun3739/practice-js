6a0e224c7b9f0891e760fd986ec7dc434970ff36
mallikarjun3739 <mallikarjun16455@gmail.com>
2022-09-11 23:28:55 +0530

    okay

---
+++ main.js
@@ -0,0 +1,200 @@
+//console.log("Hello World");
+        // let firstname,lastname = "Daily tuition", Age=24;
+        // console.log(firstname);
+        // console.log(lastname);
+        // console.log(Age);
+
+        // let Goal = "Tutorial make Happy" // this is a literal = "Tutorial make Happy"
+        // let reference = Goal;
+        // console.log(Goal);
+        // console.log(reference);
+
+        // const blue = Symbol();
+        // const white = Symbol("color white");
+        // console.log(blue===white);
+
+        //primitive data type
+
+        // let Myvalue;
+
+        // Myvalue=null;
+        // Myvalue=19.4;
+        //Myvalue=undefined;
+
+        //console.log(Myvalue);
+
+        // Object
+         
+        // const obj = {
+        //     name:"Rajshekar",
+        //     age:28
+        // }
+        // console.log(obj.name);
+        // console.log(obj.age);
+
+        //Deleting object properties
+        // const obj = {
+        //     name:"Rajshekar",
+        //     age:28
+        // }
+
+        // delete(obj.age);
+        // console.log(obj.age);
+
+        // Creating a array with different data types
+            
+        // const a1 = [1,2,3,4,5,];
+        // const a2 = [1,2,null,5,'five'];
+        // const a3 = ['hello','world','array'];
+
+        //have an array containing array
+
+        // const a4 = [
+        //     [1,2,3],
+        //     [4,5,6]
+        // ];
+
+        //get array length
+
+        //console.log(a3.length);
+        // change array element
+        // a3[0]="welcome";
+        // console.log(a3[0]);
+        //console.log(a3);
+
+        //displaying date
+
+        // const now = new Date();
+        // console.log(now);
+        
+        // const dt = new Date(2018,08,09,15,00);
+        // console.log(dt);
+        
+        // representing string
+        //  let str = 'iam from,"prostack academy"';
+        //  let str1 = "iam from bijapur";
+
+         //displays error,but we have to use backslash and string literal to correct it... 
+         //let strerror = "we took back bag from him,he said \"Don\'t touch my bag\"";
+
+        // string concatenation
+        
+        // let temp = 20.5;
+        // let strcap =`weather of india is ${temp}\u00b0c`;
+
+        // console.log(strcap);
+
+        // printing next line \n 
+
+        // let str = "One line\n Second line";
+        // console.log(str);
+
+        // multiline code with back tick
+
+        // let str = `One line \n Second line`;
+        // console.log(str);
+
+        // let str1 = 5*'5';
+        // let srt2 = 5+'5';
+        // console.log(str1);
+        // console.log(str2);
+
+        // control statements
+        //if statement
+
+        // let time = 8;
+
+        // if(time<10){
+
+        //     console.log("Good Morning");
+        // }else{
+        //     console.log("Wrong time")
+        // }
+
+        // nested if and else statements
+
+        // let time = 9;
+
+        // if(time<10)
+        // {
+        //     console.log("Good Morning");
+        //     if(time==8)
+        //     {
+        //          console.log("Have a nice day");   
+        //     }
+        // }
+        // else if(time<17)
+        // {
+        //     console.log("Wrong time")
+        // }
+        // else{
+        //     console.log("go to hell");
+        // }
+        
+        //switch statement
+
+        /*
+        example
+        which language do you choose
+        1)javascript 2)python 
+        3)Java  4)Asp.net 
+
+        let ans = 1;
+
+        switch(ans){
+            case 1:
+                console.log("you have selected javascript");
+            break;
+            case 2:
+                console.log("you have selected python");
+            break;
+            case 3:
+                console.log("you have selected Java");
+            break;
+            case 4:
+                console.log("you have selected Asp.net");
+            break;
+            default:
+                console.log("youn have not selected any language");
+            break;
+            }
+                */
+
+            //while loop
+            
+            // let count = 0;
+            // while(count<10){
+            //     console.log(count);
+            //     count++;
+            // }
+
+            //do while loop
+
+            // let count = 0
+            // do{         //execute this block atleast once
+            //     console.log("count value" + count);//print count variable value on console
+            //     count++         //increase count variable value by 1
+            // }while(count<5); //evaluate expression
+
+            //for loop
+
+            /* print 0 to 4 on the console */
+            // for(let count=0; count<5; count++){
+            //     console.log("Number is "+ count);
+            // }
+
+            // for(let i=0,j=5;i<5;i++,j--){
+            //     console.log(i+" and "+ j);
+            // }
+            
+            // for in loop
+
+            // const obj = {a:5,b:6,c:8,h:10}
+
+            // for(let o in obj)
+            // {
+            //     // console.log("o" + obj[o]);
+            //     // console.log(obj[o]);
+            //     console.log(o + " : " + obj[o]);
+                
+            // }
